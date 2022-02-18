b1a5969262cff783c07d004a46609992c185703e
Martin A. Jackson <martin.a.jackson>
2022-02-18 11:41:50 -0600

    working test1

--- server/db/test1.js
+++ server/db/test1.js
@@ -1,53 +1,36 @@
 
 import { MongoClient } from "mongodb"
-import dotenv from 'dotenv'
 
-dotenv.config()
-
-const cfg = {
-    user: process.env.User,
-    pass: process.env.Password,
-};
-
-// console.table(cfg);
-
-// const uri = `mongodb://${cfg.user}:${cfg.pass}@localhost:27017`;
-const uri = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
+const uri = 'mongodb://localhost:27017/?readPreference=primary&directConnection=true&ssl=false'
 const client = new MongoClient(uri);
 
 // -----------------------------------------------------------------------------------------------
-async function run() {
+async function query(dbName, collectionName, where) {
 
-    console.log('db-specific.js run() started');
+    // const where = {title: /.*Star Wars.*/}
+    // const where1 = {movie_id: 11}
+
+    const result = []
+
     try {            
-      console.log('-1. run()')
       await client.connect()     
 
-      console.log('0. run()');
+      const collection = client.db(dbName).collection(collectionName)
+      const cursor = collection.find(where);
 
-      const db = client.db("movies");
-      console.log('1. run()');
+      await cursor.forEach(rec => result.push(rec));       // does not work without await    
 
-      const result = db.collection('movie').find({movie_id: 11});
-      result.forEach(console.log)
+      await client.close()     
+      return result
 
-      console.log('4. run() finally');
-      // console.log('ser stat:', db.serverStatus());
-      await client.close();
-
-  } catch (err) {
-        console.log('3. run() Err:', err);
+    } catch (err) {
+        console.log('Err:', err);
         throw err;
     } 
+
 }
   
 
-  try {
-      run()
-  } catch(err) {
-      console.log('startup db-specific.js run() failed.');
-      console.dir(err)
-  } finally { 
-      console.log("run() finally") 
-  }; 
-
+query("movies", 'movie', {title: /.*Star Wars.*/})
+   .then( result => result.map(r => console.dir(r.title)) )
+   .catch(console.dir);
